eceff00f69717c79f1a5ecca53bff843e5e6e94b
FerdouseOfficial <mollajihad402@gmail.com>
2026-03-20 20:07:14 +0600

    It was a mistake I commit the false one. Now I am practicing the conept of the props actually revising okay. I think its clear

--- Practice Project/src/App.jsx
+++ Practice Project/src/App.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 
 const App = () => {
   return (
-    
+    <div>
+      <User name="Mianur" age="20" isMarried="false" hobby="not reading, sleeping, money"  />
+      <User name="Mianur" age="20" isMarried="false" hobby="not reading, sleeping, money" />
+    </div>
   )
 }
 
 const User = (props) => {
   return(
-    <h2></h2>
+    <div>
+      <h2> {props.name} </h2>
+      <p> {props.age} </p>
+      <p> {props.isMarried} </p>
+      <p> {hobby} </p>
+    </div>
   )
 }
 
